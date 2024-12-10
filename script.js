91ba56792a10b9773c3c5c2b37f788a738616a08
TheItsNamless <itsnameless@outlook.com>
2024-12-10 16:43:28 +0100

    Aufgabe 4

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Projektdetails
+let projektInfos = ['Projekt 1 ist Toll!', 'Projekt 2 ist noch besser!'];
+
+function zeigeProjektDetails(event, index, projectDetails) {
+    projectDetails.textContent = projektInfos[index];
+}
+
+let projectsList = document.querySelector('#projects');
+let projects = projectsList.querySelectorAll('li');
+let projectDetails = document.querySelector('#details');
+
+for (let i = 0; i < projects.length; i++) {
+    projects[i].addEventListener('click', (event) =>
+        zeigeProjektDetails(event, i, projectDetails)
+    );
+}
+
+// Formularvalidierung
+function validiereFormular() {
+    let name = document.querySelector('#name');
+    let nachricht = document.querySelector('#nachricht');
+
+    if (name.value == '' || nachricht.value == '') {
+        alert('Bitte fülle die Felder aus!');
+        return false;
+    }
+
+    return true;
+}
